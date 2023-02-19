3142f33882388d9738fb6a084d66d966409d6499
Ashishtrick <93420617+Ashishtrick@users.noreply.github.com>
2023-02-19 19:27:32 +0530

    small commit

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -3,10 +3,10 @@ import Pages from "./pages/Pages";
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
       <Pages />
     </div>
   );
 }
+
 
 export default App;
--- recipe-app/src/components/Popular.jsx
+++ recipe-app/src/components/Popular.jsx
@@ -12,20 +12,21 @@ function Popular() {
 
   const getPopular = async () => {
 
-
     const check = localStorage.getItem("popular");
     if (check) {
-        setPopular(JSON.parse(check));
-    } else{
-        const api = await fetch(
-            `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-          );
-          const data = await api.json();
+      setPopular(JSON.parse(check));
+    }else{
+      const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+    );
+    const data = await api.json();
 
-            localStorage.setItem("popular", JSON.stringify(data.recipes));
-          setPopular(data.recipes);
-          console.log(data.recipes);
-    } 
+    localStorage.setItem("popular", JSON.stringify(data.recipes));
+    setPopular(data.recipes);
+    console.log(data.recipes);
+  }
+
+    
   };
   return (
     <div>
@@ -38,7 +39,7 @@ function Popular() {
             arrows: false,
             pagination: false,
             drag: "free",
-            gap: "5rem",
+            gap: "3rem",
           }}
         >
           {popular.map((recipe) => {
@@ -47,7 +48,7 @@ function Popular() {
                 <Card>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title} />
-                    <Gradient />
+                  <Gradient />
                 </Card>
               </SplideSlide>
             );
@@ -63,7 +64,8 @@ const Wrapper = styled.div`
 `;
 
 const Card = styled.div`
-  min-height: 25rem;
+  min-width: 12rem;
+  height: 15rem;
   border-radius: 2rem;
   overflow: hidden;
   position: relative;
@@ -76,7 +78,6 @@ const Card = styled.div`
     height: 100%;
     object-fit: cover;
   }
-  
   p {
     position: absolute;
     z-index: 10;
@@ -99,7 +100,7 @@ const Gradient = styled.div`
   position: absolute;
   z-index: 3;
   width: 100%;
-    height: 100%;
-    background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
-  `
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`;
 export default Popular;
